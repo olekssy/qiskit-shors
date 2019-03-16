e2dc6b2753cde16bde963a2956f28189d14fcbda
Oleksii Lialka <lialka@ualberta.ca>
2019-03-16 00:41:27 -0400

    Added RecursionError and OverflowError handling
    Signed-off-by: Oleksii Lialka <lialka@ualberta.ca>

--- assets/FactorClass.py
+++ assets/FactorClass.py
@@ -1,3 +1,26 @@
+#  MIT License
+#
+#  Copyright (c) 2019 Oleksii Lialka
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+import sys
 import warnings
 import random
 
@@ -9,6 +32,8 @@ def sample(n, sample_size):
     low = 2
     top = 4096
     sample = []
+    if top > n:
+        top = n
     for i in range(sample_size):
         integer = random.randrange(low, top)
         sample.append(integer)
@@ -27,9 +52,17 @@ def gcd(a, b):
 def euclidean(n, int_set):
     """Factoring composite int via Euclidean algorithm"""
     for a in int_set:
-        p = gcd(n, a)
-        if 1 < p < n:
-            return p
+        try:
+            p = gcd(n, a)
+            if 1 < p < n:
+                return p
+        except RecursionError as message:
+            recursion_limit = sys.getrecursionlimit()
+            print(message, recursion_limit)
+            sys.setrecursionlimit(recursion_limit * 2)
+            print('Recursion depth limit is set to', sys.getrecursionlimit(
+            ))
+            continue
 
 
 # TODO period func, which returns list of periods that matches
@@ -54,6 +87,7 @@ def period(n, a):
 def modexp(n, int_set):
     """Function computes p * q = N from the period of the mod exp
     function"""
+    global q
     for a in int_set:
         r = period(n, a)
         if r is None:
@@ -66,7 +100,4 @@ def modexp(n, int_set):
             except RuntimeWarning:
                 print("RuntimeWarning: int64 < [{} exp "
                       "{}]".format(a, int(r / 2)))
-                continue
-            except ValueError as message:
-                print(message)
                 continue
--- shors.py
+++ shors.py
@@ -1,3 +1,25 @@
+#  MIT License
+#
+#  Copyright (c) 2019 Oleksii Lialka
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
 import random
 import sys
 from assets import FactorClass
@@ -6,45 +28,36 @@ from assets import FactorClass
 order = 3
 limit = 10 ** order  # upper limit of the integer to be factored
 sample_size = 10  # num of integers in a sample for finding a period
-recursion_limit = 1000
-iterations = 3
+recursion_limit = 1000  # sys default set to 1000
+seed = 3
 
 
-def main(limit, sample_size):
+def main():
+    n = random.randint(10 ** (order - 1), limit)
+    randint_set = FactorClass.sample(n, sample_size)
+    p = FactorClass.euclidean(n, randint_set)
     try:
-        n = random.randint(10 ** (order - 1), limit)
-        randint_set = FactorClass.sample(n, sample_size)
-        p = FactorClass.euclidean(n, randint_set)
         if p is not None:
             q = int(n / p)
         else:
             print("-> Euclidean algorithm failed")
-            # proceed to modular exponentiation
             p = FactorClass.modexp(n, randint_set)
             if p is not None:
                 q = int(n / p)
             else:
-                print("-> Modexp algorithm failed")
                 p, q = 1, n
-    except KeyboardInterrupt:
-        p, q = 1, n
-        print('\n-> Program interrupted by User')
-        quit()
+                print("-> Classical Modexp failed")
     except OverflowError as message:
         q = 'q'
         print('Unable to find second factor q = n / p, because', message)
-    except RecursionError as message:
-        p, q = 1, n
-        recursion_limit = sys.getrecursionlimit()
-        print(message, recursion_limit)
-        print('If exception persists, increase the recursion depth '
-              'limit manually in Global variables')
-    finally:
-        print("{} x {} = {}\n".format(p, q, n))
+        if p is None:
+            p = 'p'
+
+    print("{} x {} = {}".format(p, q, n))
 
 
 if __name__ == '__main__':
     print()
     sys.setrecursionlimit(recursion_limit)
-    for i in range(iterations):
-        main(limit, sample_size)
+    for i in range(seed):
+        main()
