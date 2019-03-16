db78b98a28172348352887ec6f36083681945e49
Oleksii Lialka <lialka@ualberta.ca>
2019-03-16 00:42:58 -0400

    More efficient code
    Signed-off-by: Oleksii Lialka <lialka@ualberta.ca>

--- assets/FactorClass.py
+++ assets/FactorClass.py
@@ -1,89 +1,39 @@
-#  MIT License
-#
-#  Copyright (c) 2019 Oleksii Lialka
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE.
-
-import sys
-import warnings
+import math
 import random
 
-warnings.filterwarnings('error', "", RuntimeWarning)
+from numpy import random
 
 
 def sample(n, sample_size):
     """Define sample of int divisors for finding period"""
     low = 2
-    top = 4096
-    sample = []
+    top = 512
     if top > n:
         top = n
-    for i in range(sample_size):
-        integer = random.randrange(low, top)
-        sample.append(integer)
+    sample = random.randint(low, top, size=sample_size)
     return sorted(sample)
-
-
-def gcd(a, b):
-    """Euclidean algorith of finding GCD"""
-    if b > a:
-        return gcd(b, a)
-    if a % b == 0:
-        return b
-    return gcd(b, a % b)
 
 
 def euclidean(n, int_set):
     """Factoring composite int via Euclidean algorithm"""
     for a in int_set:
-        try:
-            p = gcd(n, a)
-            if 1 < p < n:
-                return p
-        except RecursionError as message:
-            recursion_limit = sys.getrecursionlimit()
-            print(message, recursion_limit)
-            sys.setrecursionlimit(recursion_limit * 2)
-            print('Recursion depth limit is set to', sys.getrecursionlimit(
-            ))
-            continue
+        p = math.gcd(n, a)
+        if 1 < p < n:
+            return p
 
 
-# TODO period func, which returns list of periods that matches
-#  co-primes and make possible to select the smallest r for a**r
 def period(n, a):
-    """Compute the period of a^kmod n)"""
-    remainder_list = []
-    k = 1
-    while True:
-        r = len(remainder_list)
-        if r > 128:
+    """Compute period of a^kmod n)"""
+    r = 1
+    modexp = a
+    while modexp != 1:
+        r += 1
+        modexp = (modexp * a) % n
+        if r > 64:
             return None
-        remainder = a ** k % n
-        if remainder in remainder_list:
-            return r
-        else:
-            remainder_list.append(remainder)
-            k += 1
+    return r
 
 
-# TODO set list of periods as arg of modexp(), s.t. can parse from QFT
 def modexp(n, int_set):
     """Function computes p * q = N from the period of the mod exp
     function"""
@@ -93,11 +43,6 @@ def modexp(n, int_set):
         if r is None:
             continue
         elif r % 2 == 0:
-            try:
-                p = gcd(n, int(a ** (r / 2) - 1))
-                if p != 1 and p != n:
-                    return p
-            except RuntimeWarning:
-                print("RuntimeWarning: int64 < [{} exp "
-                      "{}]".format(a, int(r / 2)))
-                continue
+            p = math.gcd(n, int(a ** (r / 2) - 1))
+            if p != 1 and p != n:
+                return p
--- shors.py
+++ shors.py
@@ -1,34 +1,11 @@
-#  MIT License
-#
-#  Copyright (c) 2019 Oleksii Lialka
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#  SOFTWARE.
-
 import random
-import sys
+
 from assets import FactorClass
 
 # Global variables
 order = 3
 limit = 10 ** order  # upper limit of the integer to be factored
 sample_size = 10  # num of integers in a sample for finding a period
-recursion_limit = 1000  # sys default set to 1000
 seed = 3
 
 
@@ -36,28 +13,21 @@ def main():
     n = random.randint(10 ** (order - 1), limit)
     randint_set = FactorClass.sample(n, sample_size)
     p = FactorClass.euclidean(n, randint_set)
-    try:
+    if p is not None:
+        q = int(n / p)
+    else:
+        print("-> Euclidean algorithm failed")
+        p = FactorClass.modexp(n, randint_set)
         if p is not None:
             q = int(n / p)
         else:
-            print("-> Euclidean algorithm failed")
-            p = FactorClass.modexp(n, randint_set)
-            if p is not None:
-                q = int(n / p)
-            else:
-                p, q = 1, n
-                print("-> Classical Modexp failed")
-    except OverflowError as message:
-        q = 'q'
-        print('Unable to find second factor q = n / p, because', message)
-        if p is None:
-            p = 'p'
+            p, q = 1, n
+            print("-> Classical Modexp failed")
 
     print("{} x {} = {}".format(p, q, n))
 
 
 if __name__ == '__main__':
     print()
-    sys.setrecursionlimit(recursion_limit)
     for i in range(seed):
         main()
